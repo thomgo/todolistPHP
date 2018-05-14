3af7473812bf4a7be1d8a4ea2a390a81f14e5688
Thomas <tg.59290@gmail.com>
2018-05-14 12:04:05 +0200

    boilerplate operationnel

--- views/indexVue.php
+++ views/indexVue.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <p>C'est la vue de l'index</p>
-    <?php echo $my_client->getProducts()[0]->getName(); ?>
-  </body>
-</html>
+<?php
+  include("template/header.php")
+ ?>
+
+<p>This is the index view</p>
+
+ <?php
+   include("template/footer.php")
+  ?>

ffac6c3f44a466cc9b08477d750d4e967194d77a
Thomas <tg.59290@gmail.com>
2018-05-27 23:28:06 +0200

    basic style for date display on indexview

--- views/indexView.php
+++ views/indexView.php
@@ -44,8 +44,10 @@
    ?>
    <div class="col-sm-4">
      <article class="project secondColor my-3 py-2 px-2">
-       <p><?php echo $project["endDate"]; ?></p>
-       <p><?php echo $project["leftDays"]; ?></p>
+       <p class="date">
+         <span><i class="fas fa-calendar-alt"></i><?php echo " J -" . $project["leftDays"]; ?></span>
+         <span><?php echo "(" . $project["endDate"] . ")"; ?></span>
+       </p>
        <h3><?php echo $project["title"]; ?></h3>
        <p><?php echo $project["description"]; ?></p>
        <div class="actions flex">
